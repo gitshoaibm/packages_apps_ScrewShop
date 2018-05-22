0f18d43ceb0f02d795f63e3e4e0b80bb21ffd6b8
Shoaib <shoaib.gcpdev@gmail.com>
2018-05-22 19:01:42 +0000

    HeadsUp: add timeout option (2/2)
    Forward ported to marshmallow/nougat By: @BeansTown106
    removed the none option as we now have a headsup minimum time of 2seconds
    added 5sec option as that is the new default in marshmallow
    
    Conflicts:
    	res/values-ru/nitrogen_strings.xml
    	res/values/nitrogen_arrays.xml
    	res/values/nitrogen_strings.xml
    
    kufikugel authored and sagarshah1729 committed on Nov 3, 2016

--- src/com/mrapocalypse/screwdshop/frags/HeadsUp.java
+++ src/com/mrapocalypse/screwdshop/frags/HeadsUp.java
@@ -23,7 +23,9 @@ import android.content.DialogInterface;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceGroup;
 import android.support.v7.preference.PreferenceScreen;
@@ -50,9 +52,10 @@ import java.util.List;
 import java.util.Map;
 
 public class HeadsUp extends SettingsPreferenceFragment
-        implements Preference.OnPreferenceClickListener {
+        implements Preference.OnPreferenceClickListener, Preference.OnPreferenceChangeListener {
 
     private static final int DIALOG_BLACKLIST_APPS = 0;
+    private static final String PREF_HEADS_UP_TIME_OUT = "heads_up_time_out";
 
     private static final String KEY_HEADS_UP_NOTIFICATIONS_ENABLED = "heads_up_notifications_enabled";
 
@@ -60,6 +63,7 @@ public class HeadsUp extends SettingsPreferenceFragment
     private PackageManager mPackageManager;
     private PreferenceGroup mBlacklistPrefList;
     private Preference mAddBlacklistPref;
+    private ListPreference mHeadsUpTimeOut;
 
     private String mBlacklistPackageList;
     private Map<String, Package> mBlacklistPackages;
@@ -86,6 +90,36 @@ public class HeadsUp extends SettingsPreferenceFragment
 
         mHeadsUpNotificationsEnabled = (GlobalSettingSwitchPreference) findPreference(KEY_HEADS_UP_NOTIFICATIONS_ENABLED);
         updatePrefs();
+
+        Resources systemUiResources;
+        try {
+            systemUiResources = getPackageManager().getResourcesForApplication("com.android.systemui");
+        } catch (Exception e) {
+            return;
+        }
+
+        int defaultTimeOut = systemUiResources.getInteger(systemUiResources.getIdentifier(
+                    "com.android.systemui:integer/heads_up_notification_decay", null, null));
+        mHeadsUpTimeOut = (ListPreference) findPreference(PREF_HEADS_UP_TIME_OUT);
+        mHeadsUpTimeOut.setOnPreferenceChangeListener(this);
+        int headsUpTimeOut = Settings.System.getInt(getContentResolver(),
+                Settings.System.HEADS_UP_TIMEOUT, defaultTimeOut);
+        mHeadsUpTimeOut.setValue(String.valueOf(headsUpTimeOut));
+        updateHeadsUpTimeOutSummary(headsUpTimeOut);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mHeadsUpTimeOut) {
+            int headsUpTimeOut = Integer.valueOf((String) newValue);
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.HEADS_UP_TIMEOUT,
+                    headsUpTimeOut);
+            updateHeadsUpTimeOutSummary(headsUpTimeOut);
+            return true;
+        }
+        return false;
+
     }
 
     @Override
